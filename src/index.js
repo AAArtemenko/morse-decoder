846947da00a25a50a2b8f8edcda19f56a8b16eb9
AAARTEMENKO <ar4ys@yandex.ru>
2020-03-22 18:32:52 +0300

    feat: complete task morse-decoder

--- src/index.js
+++ src/index.js
@@ -37,8 +37,27 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(expr) {    
+    let sentence = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        let word = expr.slice(i, i + 10);        
+        let char = '';
+        if (word != '**********') {
+            for (let j = 0; j < word.length; j += 2) {
+                if (word[j] == '1') {
+                    if (word[j+1] == '0') {
+                        char += '.';
+                    }
+                    else if (word[j+1] == '1') {
+                        char += '-';
+                    }
+                }
+            }
+            sentence += MORSE_TABLE[char];
+        }
+        else {sentence += ' ';}
+    }
+    return sentence;
 }
 
 module.exports = {
